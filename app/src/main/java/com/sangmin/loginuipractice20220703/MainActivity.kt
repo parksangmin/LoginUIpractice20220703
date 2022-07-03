b819d9a16c344d822ac6082b7d66603d7be32841
박 상 민 <dhwkffks7@naver.com>
2022-07-03 15:20:07 +0900

    회원가입 로직을 완성

--- app/src/main/java/com/sangmin/loginuipractice20220703/MainActivity.kt
+++ app/src/main/java/com/sangmin/loginuipractice20220703/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sangmin.loginuipractice20220703
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
 // toast  같은 경우에는 로그와 달리, 화면의 문장을 보여준다(사용자가 알 수 있다)
 //  간단한 알림!
 
+        singUpBtn.setOnClickListener {
+            // 비행기 티켓을 발권(출발지 : this(MainActivity -> 목적지(SignUpActivity)
+            val myIntent = Intent(this,SignUpActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        loginBtn.setOnClickListener {  }
 
 
 
--- app/src/main/java/com/sangmin/loginuipractice20220703/SignUpActivity.kt
+++ app/src/main/java/com/sangmin/loginuipractice20220703/SignUpActivity.kt
@@ -2,10 +2,22 @@ package com.sangmin.loginuipractice20220703
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.singUpBtn
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+//       회원 가입 버튼 눌렀을때의 로직 완성
+        singUpBtn.setOnClickListener {
+            // 회원가입시 서버와 통신하는 API 로직
+
+            Toast.makeText(this, "${nickEdt.text}님 회원가입을 축하합니다", Toast.LENGTH_SHORT).show()
+
+            finish()
+        }
     }
 }
