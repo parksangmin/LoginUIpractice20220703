6f53e5c837b2f723ec76917e64ead95cc40397fb
박 상 민 <dhwkffks7@naver.com>
2022-07-03 10:08:46 +0900

    Initial commit

---
+++ app/src/main/java/com/sangmin/loginuipractice20220703/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sangmin.loginuipractice20220703
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
